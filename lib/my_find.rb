14cd477e9c704949001c01a68f6d762638055504
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-14 13:40:16 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,16 @@
-def my_find(collection)
-    i = 0 
-    while i < collection.length
-        return collection[i]
-    if yield(collection[i])
-        i = i + 1
-    end 
-    end 
-end 
+require 'pry'
 
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+end
